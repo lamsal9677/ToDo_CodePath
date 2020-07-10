7d45def2f94ccee3ae413c5e57d0fadefe739956
Sanskar Lamsal <lamsal9677@yahoo.com>
2020-07-10 15:01:29 -0400

    Front_UI_Ready

--- app/src/main/java/com/example/todocodepath/MainActivity.java
+++ app/src/main/java/com/example/todocodepath/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.todocodepath;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    String date = String.valueOf(android.text.format.DateFormat.format("dd-MM-yyyy", new java.util.Date()));
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text = findViewById(R.id.DateView);
+        text.setText(date);
     }
 }
